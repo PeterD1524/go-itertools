bfdcf7afdde350341b3fce1bd0fb6ef0450c6d22
PeterD1524 <qaz246135@gmail.com>
2025-02-14 18:33:28 +0800

    add Map func
    Signed-off-by: PeterD1524 <qaz246135@gmail.com>

--- itertools/itertools.go
+++ itertools/itertools.go
@@ -1,1 +1,13 @@
 package itertools
+
+import "iter"
+
+func Map[V any, B any](seq iter.Seq[V], f func(V) B) iter.Seq[B] {
+	return func(yield func(B) bool) {
+		for v := range seq {
+			if !yield(f(v)) {
+				return
+			}
+		}
+	}
+}
