e4925cca9adbcf2092c14864fdf47f8a8d921da4
PeterD1524 <qaz246135@gmail.com>
2025-02-14 19:31:57 +0800

    add All func
    Signed-off-by: PeterD1524 <qaz246135@gmail.com>

--- itertools/itertools.go
+++ itertools/itertools.go
@@ -2,6 +2,15 @@ package itertools
 
 import "iter"
 
+func All[V any](seq iter.Seq[V], f func(V) bool) bool {
+	for b := range Map(seq, f) {
+		if !b {
+			return false
+		}
+	}
+	return true
+}
+
 func Map[V any, B any](seq iter.Seq[V], f func(V) B) iter.Seq[B] {
 	return func(yield func(B) bool) {
 		for v := range seq {
